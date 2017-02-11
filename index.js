4c6e86f2b522da5c7b949368899b1d072934d696
zhangGuojin <zhanggjjim@163.com>
2017-02-11 19:24:44 +0800

    spider first page

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 import Crawler from 'crawler';
 
 const c = new Crawler({
-  maxConnections: 10,
+  method: 'GET',
+  uri: 'http://www.itjuzi.com/investevents',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+  },
+  maxRedirects: 100,
+  jar: true,
+  maxConnections: 20,
   // This will be called for each crawled page
   callback: (err, res, done) => {
-    // if (err) {
-    //   console.log(error);
-    // } else {
-    //   // const $ = res.$;
-    //   // $ is Cheerio by default
-    //   // a lean implementation of core jQuery designed specifically for the server
-    //   // console.log($("title").text());
-    // }
     if (err) {
       console.log('Crawler.error', err);
       return done();
     }
     console.log('Crawler.success');
-    console.log(res);
+    const $ = res.$;
+    const liEleArr = $('i.cell.pic a');
+    const companySiteArr = liEleArr.map(function each() {
+      return $(this).attr('href');
+    }).get();
+    console.log(companySiteArr);
     return done();
   }
 });
 
-c.queue('http://www.itjuzi.com/');
+c.queue('http://www.itjuzi.com/investevents');
--- test/test.js
+++ test/test.js
@@ -1,12 +1,22 @@
 import test from 'ava';
-import cy from './../index';
+import rq from 'request-promise';
 
-test('first', t => {
+test('findByIds', async t => {
+  const options = {
+    method: 'GET',
+    uri: 'https://www.itjuzi.com/',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+    },
+    maxRedirects: 100,
+    jar: true
+  };
   try {
-    
-    t.truthy(true);
+    const result = await rq(options);
+    console.log('result', result);
+    t.truthy(result);
   } catch (err) {
-    console.log('ava.error', err);
+    console.log('err------>', err);
     t.falsy(false);
   }
 });
