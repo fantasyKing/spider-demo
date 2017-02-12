e027452d9e952fd8b54c4fbd9fd1981c5f02a1a6
zhangGuojin <zhanggjjim@163.com>
2017-02-12 14:44:24 +0800

    fix bug

--- utils/urlUtil.js
+++ utils/urlUtil.js
@@ -1,6 +1,6 @@
 export default new class {
   complete = (url, root) => {
-    if (url && url.search(/^http:/i) === -1) {
+    if (url && url.search(/^http/i) === -1) {
       if (root.endsWith('/')) {
         url = `${this.removeLast(root)}${url}`;
       } else {
